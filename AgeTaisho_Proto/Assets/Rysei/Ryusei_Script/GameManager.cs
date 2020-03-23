e63a80b4d4a0c460102383eb22935519c62f82e8
ellio001 <11806042@stu.kbc.ac.jp>
2020-03-23 11:57:53 +0900

    GameManagerに手を加えました。

--- AgeTaisho_Proto/Assets/Rysei/Ryusei_Script/GameManager.cs
+++ AgeTaisho_Proto/Assets/Rysei/Ryusei_Script/GameManager.cs
@@ -2,31 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
+
+    float GameTime;             //ゲームのプレイ時間
+    float GameFinishTime;       //ゲームのプレイ最大時間3分は10,800フレーム
+    float FiverTime;            //フィーバータイム
+    float FiverFinishTime;      //フィーバータイム最大時間
+    public bool FiverFlag;      //フィーバーのフラグ
+    bool TestSceneFlag;         //デバッグ用のフラグ
+
     public static GameManager instance = null;
     public int score_num = 0; // スコア変数
     public GameObject score_object = null; // Textオブジェクト
 
     private void Awake()    //スタートよりも最初に呼ばれる
     {
-        if (instance == null)
-        {
+        if (instance == null) {
             instance = this;
             DontDestroyOnLoad(this.gameObject);
         }
-        else
-        {
+        else {
             Destroy(this.gameObject);
         }
     }
 
-    private void Update()
-    {
-        // オブジェクトからTextコンポーネントを取得
-        Text score_text = score_object.GetComponent<Text>();
-        // テキストの表示を入れ替える
-        score_text.text = "Score:" + score_num;
+    private void Start() {
+        GameTime = 0f;
+        GameFinishTime = 1000f;
+        FiverTime = 0f;
+        FiverFinishTime = 50f;
+        FiverFlag = false;
+        TestSceneFlag = true;
+    }
+
+    private void Update() {
+
+        if (TestSceneFlag) {
+            // オブジェクトからTextコンポーネントを取得
+            Text score_text = score_object.GetComponent<Text>();
+            // テキストの表示を入れ替える
+            score_text.text = "Score:" + score_num;
+            Addition();
+            Judgment();
+        }
+    }
+
+    public void Addition() {
+        //ゲーム時間を加算
+        GameTime++;
+        //Debug.Log("GameTime:" + GameTime);
+    }
+
+    public void Judgment() {
+        //ゲーム時間を判定
+        if (GameTime == GameFinishTime) {
+            Debug.Log("ゲーム終了！");
+            //Scene読込
+            ReadScene();
+        }
+        //Fiverかどうか判定
+        if (FiverFlag == false) {
+            //FiverTime初期化
+            Initial();
+            //FiverFlag = true;
+            //Debug.Log("フィーバータイム開始！");
+        }
+        else if (FiverFlag) {
+            FiverTime++;
+            Debug.Log("FiverTime:" + FiverTime);
+            //フィーバータイム終了
+            if (FiverTime >= FiverFinishTime) {
+                FiverFlag = false;
+            }
+        }
+    }
+
+    //変数を初期化するための関数
+    public void Initial() {
+        FiverTime = 0f;
+    }
+
+    public void ReadScene() {
+        //Endシーン読込
+        SceneManager.LoadScene("EndScene", LoadSceneMode.Single);
+        //処理を二度としないようにフラグで管理
+        TestSceneFlag = false;
     }
 }
--- AgeTaisho_Proto/Assets/Tawata/RyomaScript/FeverTime.cs
+++ AgeTaisho_Proto/Assets/Tawata/RyomaScript/FeverTime.cs
@@ -1,53 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class FeverTime : MonoBehaviour {
+
     float GameTime;             //ゲームのプレイ時間
-    float GameFinishTime;       //ゲームのプレイ最大時間
+    float GameFinishTime;       //ゲームのプレイ最大時間3分は10,800フレーム
     float FiverTime;            //フィーバータイム
     float FiverFinishTime;      //フィーバータイム最大時間
-
-
-    // Start is called before the first frame update
-    void Start() {
+    public bool FiverFlag;      //フィーバーのフラグ
+    bool TestSceneFlag;         //デバッグ用のフラグ
+    
+    private void Start() {
         GameTime = 0f;
-        GameFinishTime = 200f;
+        GameFinishTime = 1000f;
         FiverTime = 0f;
-        FiverFinishTime = 200f;
+        FiverFinishTime = 50f;
+        FiverFlag = false;
+        TestSceneFlag = true;
     }
 
-    // Update is called once per frame
-    void Update() {
-        Addition();
-        Judgment();
+    private void Update() {
+        
     }
 
     public void Addition() {
         //ゲーム時間を加算
         GameTime++;
-        Debug.Log(GameTime);
+        //Debug.Log("GameTime:" + GameTime);
     }
 
     public void Judgment() {
         //ゲーム時間を判定
         if (GameTime == GameFinishTime) {
             Debug.Log("ゲーム終了！");
+            //Scene読込
             ReadScene();
         }
         //Fiverかどうか判定
-        //試しに100フレームたったらフィーバー
-        if (GameTime >= GameTime - 100f) {
-            Debug.Log("フィーバータイム開始！");
-            FiverTime++;
-            Debug.Log(FiverTime);
+        if (FiverFlag == false) {
+            //FiverTime初期化
+            Initial();
+            //FiverFlag = true;
+            //Debug.Log("フィーバータイム開始！");
         }
+        else if (FiverFlag) {
+            FiverTime++;
+            Debug.Log("FiverTime:" + FiverTime);
+            //フィーバータイム終了
+            if (FiverTime >= FiverFinishTime) {
+                FiverFlag = false;
+            }
+        }
+    }
+
+    //変数を初期化するための関数
+    public void Initial() {
+        FiverTime = 0f;
     }
 
     public void ReadScene() {
         //Endシーン読込
         SceneManager.LoadScene("EndScene", LoadSceneMode.Single);
+        //処理を二度としないようにフラグで管理
+        TestSceneFlag = false;
     }
-
 }
