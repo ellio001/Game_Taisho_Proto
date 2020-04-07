bd1011da9567f45a69792297938f1f6320c41ebd
810shoma <ow8100@gmail.com>
2020-04-07 11:22:09 +0900

    チュートリアルで指定の動きしかできないスクリプトを作成（仮）

---
+++ AgeTaisho_Proto/Assets/Tutorial/Script/Tutorials_inOrder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorials_inOrder : MonoBehaviour
+{
+    float speed_time = 10f; // 師匠が喋っている時間
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
