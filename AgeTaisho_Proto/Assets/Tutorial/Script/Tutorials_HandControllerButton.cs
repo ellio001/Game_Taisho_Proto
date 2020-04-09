fdbfd823315d8ce6d22ba94bd25c672153c7b584
ellio001 <11806042@stu.kbc.ac.jp>
2020-04-09 17:20:50 +0900

    しょうまのチュートリアルハンドコントローラーと結合

--- AgeTaisho_Proto/Assets/Tutorial/Script/Tutorials_HandControllerButton.cs
+++ AgeTaisho_Proto/Assets/Tutorial/Script/Tutorials_HandControllerButton.cs
@@ -18,6 +18,10 @@ public class Tutorials_HandControllerButton : MonoBehaviour
     GameObject C2;    // Camera_2を入れる変数
     Camera_2 C2_script; // Camera_2のscriptを入れる変数
 
+    //ポーズ画面
+    GameObject Pause;
+    Pause_Botton_Script script;
+
     public TutorialUI tutorialUI;
     int TextNumber;
 
@@ -34,77 +38,80 @@ public class Tutorials_HandControllerButton : MonoBehaviour
 
         C2 = GameObject.Find("Main Camera");
         C2_script = C2.GetComponent<Camera_2>();
+        //ポーズ画面
+        Pause = GameObject.Find("Main Camera");
+        script = Pause.GetComponent<Pause_Botton_Script>();
     }
 
     void Update()
     {
-        TextNumber = tutorialUI.TextNumber; // TextNumberの値を常に更新
+        if (script.PauseFlag) {
+            return;
+        }
+        else {
+            TextNumber = tutorialUI.TextNumber; // TextNumberの値を常に更新
 
-        Ray ray = new Ray();
-        RaycastHit hit = new RaycastHit();
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = new Ray();
+            RaycastHit hit = new RaycastHit();
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if (TextNumber == 3 || TextNumber == 4 ||
-            TextNumber == 5 || TextNumber == 7 ||
-            TextNumber == 8 || TextNumber == 9) Move_arrow(); // 矢印を表示
-       
-        // 天ぷらが生成されたら次のテキストに進む
-        if (GameObject.Find("ItemTenpura") && TextNumber == 6) tutorialUI.TextNumber = 7;
+            if (TextNumber == 3 || TextNumber == 4 ||
+                TextNumber == 5 || TextNumber == 7 ||
+                TextNumber == 8 || TextNumber == 9) Move_arrow(); // 矢印を表示
 
-        if (Physics.Raycast(ray.origin, ray.direction, out hit, 5f) && Input.GetKeyDown(KeyCode.Space) && C2_script.space_flg)
-        {
-            if (HoldingFlg != true) // 手に何も持っていない時に入る
-            {
-                if (hit.collider.gameObject.tag == "Box")
+            // 天ぷらが生成されたら次のテキストに進む
+            if (GameObject.Find("Tutorial_ItemEbi") && TextNumber == 6) tutorialUI.TextNumber = 7;
+
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, 5f) && Input.GetKeyDown(KeyCode.Space) && C2_script.space_flg) {
+                if (HoldingFlg != true) // 手に何も持っていない時に入る
                 {
-                    if (hit.collider.gameObject.name == "EbiBox" && TextNumber == 3)
-                    {
-                        Resource = (GameObject)Resources.Load("S_Resources/ItemEbi");   //Resourceフォルダのプレハブを読み込む
-                        clickedGameObject = Instantiate(Resource, ClickObj.gameObject.transform.position, Quaternion.identity); // プレハブを元にオブジェクトを生成する
+                    if (hit.collider.gameObject.tag == "Box") {
+                        if (hit.collider.gameObject.name == "EbiBox" && TextNumber == 3) {
+                            Resource = (GameObject)Resources.Load("S_Resources/Tutorial_ItemEbi");   //Resourceフォルダのプレハブを読み込む
+                            clickedGameObject = Instantiate(Resource, ClickObj.gameObject.transform.position, Quaternion.identity); // プレハブを元にオブジェクトを生成する
+                            HoldingFlg = true;
+                            tutorialUI.TextNumber = 4; // テキストを進める
+                            DestroyFlg = true; // 矢印を消すフラグを立てる
+                                               //当たり判定をを外す
+                            ColliderOut();
+                        }
+                    }
+
+                    if (hit.collider.gameObject.tag == "Item" && (TextNumber != 6 && TextNumber != 9)) {
+                        clickedGameObject = hit.collider.gameObject;                              //タグがなければオブジェクトをclickedGameObjectにいれる
+                        clickedGameObject.transform.position = ClickObj.gameObject.transform.position;  //オブジェクトを目の前に持ってくる
                         HoldingFlg = true;
-                        tutorialUI.TextNumber = 4; // テキストを進める
-                        DestroyFlg = true; // 矢印を消すフラグを立てる
                         //当たり判定をを外す
                         ColliderOut();
                     }
-                }
 
-                if (hit.collider.gameObject.tag == "Item" && (TextNumber != 6 && TextNumber != 9) )
+                }
+                else if ((TextNumber == 4 && hit.collider.gameObject.tag == "kona") || (TextNumber == 5 && hit.collider.gameObject.tag == "tenpuranabe") ||
+                         (TextNumber == 7 && hit.collider.gameObject.tag == "Sara") || (TextNumber == 8 && hit.collider.gameObject.name == "Plate2"))
+                // 粉や鍋にすでに食材があるなら食材を置けないようにしている(唐揚げは何個でも置ける)
                 {
-                    clickedGameObject = hit.collider.gameObject;                              //タグがなければオブジェクトをclickedGameObjectにいれる
-                    clickedGameObject.transform.position = ClickObj.gameObject.transform.position;  //オブジェクトを目の前に持ってくる
-                    HoldingFlg = true;
-                    //当たり判定をを外す
-                    ColliderOut();
+                    //当たり判定を入れる
+                    ColliderIn();
+                    ClickObj2.GetChild(0).gameObject.transform.position = hit.point; // 見ているところに置く
+                    clickedGameObject.transform.parent = null;              //手との親子付けを解除
+
+                    clickedGameObject.GetComponent<Rigidbody>().isKinematic = false;    //重力を有効
+
+                    clickedGameObject = null;   //対象を入れる箱を初期化
+                    Resource = null;            //生成するプレハブの箱を初期化
+
+                    HoldingFlg = false;
+                    tutorialUI.TextNumber += 1; // テキストを進める
+                    DestroyFlg = true;
+                }
+                if (clickedGameObject != null)  //nullでないとき処理
+                {
+                    clickedGameObject.transform.parent = ClickObj.gameObject.transform; //このスクリプトが入っているオブジェクトと親子付け
+                    clickedGameObject.GetComponent<Rigidbody>().isKinematic = true; //ヒットしたオブジェクトの重力を無効
                 }
 
             }
-            else if ((TextNumber == 4 && hit.collider.gameObject.tag == "kona") || (TextNumber == 5 && hit.collider.gameObject.tag == "tenpuranabe")||
-                     (TextNumber == 7 && hit.collider.gameObject.tag == "Sara") || (TextNumber == 8 && hit.collider.gameObject.name == "Plate2"))
-            // 粉や鍋にすでに食材があるなら食材を置けないようにしている(唐揚げは何個でも置ける)
-            {
-                //当たり判定を入れる
-                ColliderIn();
-                ClickObj2.GetChild(0).gameObject.transform.position = hit.point; // 見ているところに置く
-                clickedGameObject.transform.parent = null;              //手との親子付けを解除
-
-                clickedGameObject.GetComponent<Rigidbody>().isKinematic = false;    //重力を有効
-
-                clickedGameObject = null;   //対象を入れる箱を初期化
-                Resource = null;            //生成するプレハブの箱を初期化
-
-                HoldingFlg = false;
-                tutorialUI.TextNumber += 1; // テキストを進める
-                DestroyFlg = true;
-            }
-            if (clickedGameObject != null)  //nullでないとき処理
-            {
-                clickedGameObject.transform.parent = ClickObj.gameObject.transform; //このスクリプトが入っているオブジェクトと親子付け
-                clickedGameObject.GetComponent<Rigidbody>().isKinematic = true; //ヒットしたオブジェクトの重力を無効
-            }
-            
         }
-
     }
 
     //当たり判定を切る関数
