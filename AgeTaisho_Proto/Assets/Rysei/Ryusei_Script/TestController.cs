163fa89bfe1c48fb829d682b47888673bc935063
nishishimamoto <11807020@stu.kbc.ac.jp>
2020-04-13 20:09:35 +0900

    コントローラ対応

---
+++ AgeTaisho_Proto/Assets/Rysei/Ryusei_Script/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("〇")) Debug.Log("〇ボタン");
+        if (0 < Input.GetAxisRaw("Cross_Horizontal")) Debug.Log("十字キー右");
+        else if (0 > Input.GetAxisRaw("Cross_Horizontal")) Debug.Log("十字キー左");
+        if (0 < Input.GetAxisRaw("Cross_Vertical")) Debug.Log("十字キー上");
+        else if (0 > Input.GetAxisRaw("Cross_Vertical")) Debug.Log("十字キー下");
+    }
+}
