432bba65a6a849d15cc24914b896dc17439e1a9a
ellio001 <11806042@stu.kbc.ac.jp>
2020-05-01 09:56:55 +0900

    本日のおすすめ

---
+++ AgeTaisho_Proto/Assets/Tawata/RyomaScript/Roulette.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roulette : MonoBehaviour {
+
+    //追加
+    float speed = 0;
+    
+    //Start is called before the first frame update
+    void Start() {    
+    }
+
+    //Update is called once per frame
+    private void Update() {
+
+        //追加
+        if (Input.GetMouseButtonDown(0)) {
+            this.speed = -100;
+        }
+        transform.Rotate(0, 0, this.speed);
+        this.speed *= 0.96f;
+    }
+
+}
