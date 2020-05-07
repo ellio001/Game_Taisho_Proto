af535471f6988340b37b6ecb85967e355cf386a4
nishishimamoto <11807020@stu.kbc.ac.jp>
2020-05-07 16:08:07 +0900

    客と客生成処理の変更

--- AgeTaisho_Proto/Assets/Rysei/Guest_Script/TestGuestGenerator.cs
+++ AgeTaisho_Proto/Assets/Rysei/Guest_Script/TestGuestGenerator.cs
@@ -8,6 +8,7 @@ public class TestGuestGenerator : MonoBehaviour
     public int Trainnumber;         //客の種類
     public int Positionnumber;
     public int GuestSpawn;         //客生成の間隔
+    int GuestSpawnCount;            //客が生成された回数
 
     public GameObject[] GuestType;  //客の種類
     public Vector3[] Position;      //席の番号(座標)
@@ -27,6 +28,7 @@ public class TestGuestGenerator : MonoBehaviour
         FirstGuest = false;
         GM = GameObject.Find("GameManager");
         script = GM.GetComponent<GameManager>();
+        GuestSpawnCount = 0;
     }
 
     // Update is called once per frame
@@ -45,13 +47,25 @@ public class TestGuestGenerator : MonoBehaviour
 
         if (time >= GuestSpawn && Guest[Guest.Length - 1] == null)   //列の最後尾が埋まってなければGuestSpawn秒に１回処理
         {
+
+            if (GuestSpawnCount % 7 == 0) Guest[Guest.Length - 1] = Instantiate(GuestType[5], Position[Position.Length - 1], transform.rotation);  //客生成(客番号,座標,回転)
+            else if (GuestSpawnCount % 10 == 0) Guest[Guest.Length - 1] = Instantiate(GuestType[4], Position[Position.Length - 1], transform.rotation);  //客生成(客番号,座標,回転)
+            else if (GuestSpawnCount % 6 == 0) Guest[Guest.Length - 1] = Instantiate(GuestType[3], Position[Position.Length - 1], transform.rotation);  //客生成(客番号,座標,回転)
+            else
+            {
+                Trainnumber = Random.Range(0, 3);    //客の種類をランダムにとる
+                Guest[Guest.Length - 1] = Instantiate(GuestType[Trainnumber], Position[Position.Length - 1], transform.rotation);  //客生成(客番号,座標,回転)
+            }
+
             FirstGuest = true;
             //Trainnumber = Random.Range(0, GuestType.Length);    //客の種類をランダムにとる
-            Trainnumber = Random.Range(0, 3);    //客の種類をランダムにとる
             //Guest[Guest.Length - 1] = Instantiate(GuestType[Trainnumber], Position[Position.Length - 1], transform.rotation);  //客生成(客番号,座標,回転)
-            Guest[Guest.Length - 1] = Instantiate(GuestType[4], Position[Position.Length - 1], transform.rotation);  //客生成(客番号,座標,回転)
 
+            GuestSpawnCount += 1;
             time = 0;
+
+            Debug.Log(GuestSpawnCount);
+            Debug.Log(Trainnumber);
         }
     }
 }
---
+++ AgeTaisho_Proto/Assets/Rysei/Guest_Script/TestRuret.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TestRuret : MonoBehaviour
+{
+
+    public int RondemNumber;
+    int Flame;
+    int FlameMin;
+    int FlameMax;
+    int Count;
+    int CountMin;
+    int CountMax;
+    int RandomMin;
+    int RandomMax;
+    GameObject Recommended_object = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Recommended_object = GameObject.Find("Recommended_text"); // Textオブジェクト
+        RondemNumber = 0;
+        Flame = 0;
+        FlameMin = 0;
+        FlameMax = 200;
+        RandomMin = 100;
+        RandomMax = 200;
+        CountMax = Random.Range(RandomMin, RandomMax);
+        Count = 0;
+        CountMin = 0;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // オブジェクトからTextコンポーネントを取得
+        //Text Recommended_text = Recommended_object.GetComponent<Text>();
+
+        RondemNumber++;
+
+        if (Count++ < CountMax)
+        {
+            if (Flame++ <= FlameMax)
+            {
+                if (RondemNumber == 1)
+                {
+                    // テキストの表示を入れ替える
+                    //Recommended_text.text = "えび";
+                    print("えび");
+                }
+                else if (RondemNumber == 2)
+                {
+                    // テキストの表示を入れ替える
+                    //Recommended_text.text = "さかな";
+                    print("さかな");
+                }
+                else if (RondemNumber == 3)
+                {
+                    // テキストの表示を入れ替える
+                    //Recommended_text.text = "いも";
+                    print("いも");
+                    RondemNumber = 0;
+                }
+                Flame = FlameMin;
+            }
+        }
+        else if (Count++ >= CountMax)
+        {
+
+        }
+
+        //if (script.Number == 1) {
+        //    // テキストの表示を入れ替える
+        //    Recommended_text.text = "えび";
+        //    print("えび");
+        //}
+        //else if (script.Number == 2) {
+        //    // テキストの表示を入れ替える
+        //    Recommended_text.text = "さかな";
+        //    print("さかな");
+        //}
+        //else if (script.Number == 3) {
+        //    // テキストの表示を入れ替える
+        //    Recommended_text.text = "いも";
+        //    print("いも");
+        //}
+    }
+}
