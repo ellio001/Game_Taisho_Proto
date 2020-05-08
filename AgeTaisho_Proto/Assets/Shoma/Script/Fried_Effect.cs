3dd57b2195a03be081f580d147794b6584aa0609
810shoma <ow8100@gmail.com>
2020-05-08 13:21:40 +0900

    ・席のずれを解消

---
+++ AgeTaisho_Proto/Assets/Shoma/Script/Fried_Effect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fried_Effect : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
