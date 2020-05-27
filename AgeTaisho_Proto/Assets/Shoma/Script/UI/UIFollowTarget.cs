2fba6f1006c9d201c7ced244b645f393e4dd8a23
810shoma <ow8100@gmail.com>
2020-05-27 12:55:44 +0900

    ・UIカーソルの徐々に大きくなるバグを解消 ・UIカーソルの大きさ調整中

--- AgeTaisho_Proto/Assets/Shoma/Script/UI/UIFollowTarget.cs
+++ AgeTaisho_Proto/Assets/Shoma/Script/UI/UIFollowTarget.cs
@@ -17,6 +17,8 @@ public class UIFollowTarget : MonoBehaviour
 
     Vector2 IconSize;
     Vector3 CanvasPos;
+    Vector3 IconScale; // ひし形のアイコンのScaleを取得
+    [SerializeField] Vector3 _maxScale;
     RectTransform rect_tra;
 
     int count;
@@ -36,6 +38,8 @@ public class UIFollowTarget : MonoBehaviour
 
         C3 = GameObject.Find("Main Camera");
         C3_script = C3.GetComponent<Camera_3>();
+
+        IconScale = rectTransform.localScale;
     }
 
     void Update()
@@ -51,62 +55,62 @@ public class UIFollowTarget : MonoBehaviour
         RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRect, screenPos, uiCamera, out pos);
         rectTransform.localPosition = pos;
         
-        ScalOperation();
+        // カーソルアイコンの拡縮制御
+        rectTransform.localScale =
+            (Mathf.Sin(1 * Mathf.PI * Time.time) + 3.5f) * 0.7f * _maxScale;
+
     }
+
 
 
     void TargetSelect()
     {
-        if (tmp_cursor != C3_script.cursor || C3_script.pot_flg)
+        if (C3_script.pot_flg)
         {
-            tmp_cursor = C3_script.cursor;
-            if (C3_script.pot_flg)
-            { // 鍋の中はPcursorをターゲットにしている
-                target = C3_script.PCS_List[C3_script.Pcursor].transform;
-                if (!flg)
-                {
-                    IconSize.x = 390;
-                    IconSize.y = 170;
-                    GetComponent<RectTransform>().sizeDelta = IconSize;
-                    rect_tra.Translate(new Vector3(-0.917f, -0.815f, -0.361f));
-                    flg = true;
-                }
-            }
-            else
-            {
-                target = C3_script.Cursor_List[C3_script.cursor].transform;
-                if (flg)
-                {
-                    IconSize.x = 640;
-                    IconSize.y = 420;
-                    GetComponent<RectTransform>().sizeDelta = IconSize;
-                    rect_tra.Translate(new Vector3(+0.917f, +0.815f, +0.361f));
-                    flg = false;
-                }
-                // Saraを選択したときにカーソルが隠れないように位置調整している
-                if (C3_script.cursor == 14 || C3_script.cursor == 15)
-                {
-                    rect_tra.Translate(new Vector3(-0.917f, -0.815f, -0.361f));
-                    flg = true;
-                }
-                
-                this.transform.parent.GetComponent<RectTransform>().anchoredPosition = CanvasPos;
-            }
+            target = C3_script.PCS_List[C3_script.Pcursor].transform;
+            _maxScale = new Vector3(_maxScale.x-0.0002f, _maxScale.y - 0.0002f, _maxScale.z);
         }
+        else
+        {
+            target = C3_script.Cursor_List[C3_script.cursor].transform;
+            _maxScale = new Vector3(_maxScale.x, _maxScale.y, _maxScale.z);
+        }
+        //if (tmp_cursor != C3_script.cursor || C3_script.pot_flg)
+        //{
+        //    tmp_cursor = C3_script.cursor;
+        //    if (C3_script.pot_flg)
+        //    { // 鍋の中はPcursorをターゲットにしている
+        //        target = C3_script.PCS_List[C3_script.Pcursor].transform;
+        //        if (!flg)
+        //        {
+        //            //IconSize.x = 390;
+        //            //IconSize.y = 170;
+        //            GetComponent<RectTransform>().sizeDelta = IconSize;
+        //            //rect_tra.Translate(new Vector3(-0.917f, -0.815f, -0.361f));
+        //            flg = true;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        target = C3_script.Cursor_List[C3_script.cursor].transform;
+        //        if (flg)
+        //        {
+        //            IconSize.x = 640;
+        //            IconSize.y = 420;
+        //            GetComponent<RectTransform>().sizeDelta = IconSize;
+        //        rect_tra.Translate(new Vector3(+0.917f, +0.815f, +0.361f));
+        //        flg = false;
+        //        }
+        //        // Saraを選択したときにカーソルが隠れないように位置調整している
+        //        if (C3_script.cursor == 14 || C3_script.cursor == 15)
+        //        {
+        //            rect_tra.Translate(new Vector3(-0.917f, -0.815f, -0.361f));
+        //            flg = true;
+        //        }
+                
+        //        this.transform.parent.GetComponent<RectTransform>().anchoredPosition = CanvasPos;
+        //    }
+        //}
     }
 
-    void ScalOperation()
-    {// UIの拡大縮小をしている
-        if (count <= Count_return/2)
-        {
-            rectTransform.localScale += new Vector3(Scale_val, Scale_val, Scale_val);
-            count += 1;
-        }
-        else if (count <= Count_return)
-        {
-            rectTransform.localScale -= new Vector3(Scale_val, Scale_val, Scale_val);
-            count += 1;
-            if (count == Count_return) count = 0;
-        }
-    }
 }
