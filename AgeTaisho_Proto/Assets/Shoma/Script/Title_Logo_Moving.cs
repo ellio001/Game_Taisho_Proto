f4e60d879ea4af89b25bf00f9c1066d92f230a6c
ellio001 <11806042@stu.kbc.ac.jp>
2020-06-03 17:36:50 +0900

    タイトル修正

--- AgeTaisho_Proto/Assets/Shoma/Script/Title_Logo_Moving.cs
+++ AgeTaisho_Proto/Assets/Shoma/Script/Title_Logo_Moving.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Title_Logo_Moving : MonoBehaviour
-{
+public class Title_Logo_Moving : MonoBehaviour {
     public List<ParticleSystem> Particle_List = new List<ParticleSystem>();
     int cnt; // listの最後の要素数を入れる
     ParticleSystem.Burst burst;
@@ -18,48 +17,45 @@ public class Title_Logo_Moving : MonoBehaviour
     public float fadeSpeed = 1f; // フェード速度指定
 
 
-    void Start()
-    {
+    void Start() {
         cnt = Particle_List.Count;// listの最後の要素数を入れている
         me = this.gameObject; // 自分のオブジェクト取得
     }
 
-    void Update()
-    {
-        
-        if (fadeStart > 0f)
-            fadeStart -= Time.deltaTime;
-        else
-            if (fadeIn)
-                fadeInFunc();
+    void Update() {
 
+        if (fadeStart > 0f) {
+            fadeStart -= Time.deltaTime;
+        }
+        else {
+            if (fadeIn) {
+                fadeInFunc();
+            }
+        }
         // 徐々にエフェクトの数を減らしている
         count += 1;
-        if (count % 10 == 0 && Atai !=0)
+        if (count % 10 == 0 && Atai != 0) {
             Atai -= 0.5f;
-        
-        if(Atai == 0)
-            for (int i = 0; i <= cnt; i++)
-            {
+        }
+
+        if (Atai == 0) {
+            for (int i = 0; i < cnt; i++) {
                 // 徐々に小さくしている
                 Particle_List[i].startSize -= 0.001f;
             }
-        
+        }
 
-        for (int i = 0; i <= cnt; i++)
-        {
+        for (int i = 0; i < cnt; i++) {
             burst.count = Atai;
             Particle_List[i].emission.SetBurst(0, burst);
         }
-        
+
 
     }
 
 
-    void fadeInFunc()
-    { // フェードインの処理
-        if (me.GetComponent<Image>().color.a < 255)
-        {
+    void fadeInFunc() { // フェードインの処理
+        if (me.GetComponent<Image>().color.a < 255) {
             UnityEngine.Color tmp = me.GetComponent<Image>().color;
             tmp.a = tmp.a + (Time.deltaTime * fadeSpeed);
             me.GetComponent<Image>().color = tmp;
