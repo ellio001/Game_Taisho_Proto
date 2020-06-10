c25844f7802eb7000b16eab64c763c6ededf356d
nishishimamoto <11807020@stu.kbc.ac.jp>
2020-06-10 17:56:36 +0900

    NormalとHardのフィーバーモブの動き

--- AgeTaisho_Proto/Assets/Rysei/Ryusei_Script/FeverStaging.cs
+++ AgeTaisho_Proto/Assets/Rysei/Ryusei_Script/FeverStaging.cs
@@ -93,7 +93,7 @@ public class FeverStaging : MonoBehaviour
                         time = 0;
                     }
 
-                    if (Mobs[0].transform.position.x >= 2)
+                    if (Mobs[0].transform.position.x >= 2.5)
                         Mobs[0].transform.position += new Vector3(-0.5f, 0, 0);
 
                     if (Mobs[1].transform.position.x <= -6)
@@ -102,25 +102,25 @@ public class FeverStaging : MonoBehaviour
                     if (Mobs[2].transform.position.x >= 5)
                         Mobs[2].transform.position += new Vector3(-0.5f, 0, 0);
 
-                    if (Mobs[3].transform.position.x <= -3.5)
+                    if (Mobs[3].transform.position.x <= -12)
                         Mobs[3].transform.position += new Vector3(0.5f, 0, 0);
 
-                    if (Mobs[4].transform.position.x <= -10)
+                    if (Mobs[4].transform.position.x <= -9)
                         Mobs[4].transform.position += new Vector3(0.5f, 0, 0);
 
                 }
                 else if (GameManager.instance.FiverFlag == false)
                 {
-                    if (Mobs[0].transform.position.x <= 9)
+                    if (Mobs[0].transform.position.x <= 15)
                         Mobs[0].transform.position += new Vector3(0.5f, 0, 0);
 
                     if (Mobs[1].transform.position.x >= -17)
                         Mobs[1].transform.position += new Vector3(-0.5f, 0, 0);
 
-                    if (Mobs[2].transform.position.x <= 5.5)
+                    if (Mobs[2].transform.position.x <= 10)
                         Mobs[2].transform.position += new Vector3(0.5f, 0, 0);
 
-                    if (Mobs[3].transform.position.x >= -14)
+                    if (Mobs[3].transform.position.x >= -25)
                         Mobs[3].transform.position += new Vector3(-0.5f, 0, 0);
 
                     if (Mobs[4].transform.position.x >= -20)
@@ -128,6 +128,54 @@ public class FeverStaging : MonoBehaviour
                 }
                 break;
             case "Hard_Scene":
+
+                if (GameManager.instance.FiverFlag == true)
+                {
+                    time += Time.deltaTime;
+
+                    if (time >= 0.5)
+                    {
+                        rb[0].AddForce(transform.up * 1);
+                        rb[1].AddForce(transform.up * 1);
+                        rb[2].AddForce(transform.up * 1);
+                        rb[3].AddForce(transform.up * 1);
+                        rb[4].AddForce(transform.up * 1);
+                        time = 0;
+                    }
+
+                    if (Mobs[0].transform.position.x >= 2.5)
+                        Mobs[0].transform.position += new Vector3(-0.5f, 0, 0);
+
+                    if (Mobs[1].transform.position.x <= -2.5)
+                        Mobs[1].transform.position += new Vector3(0.5f, 0, 0);
+
+                    if (Mobs[2].transform.position.x >= 5)
+                        Mobs[2].transform.position += new Vector3(-0.5f, 0, 0);
+
+                    if (Mobs[3].transform.position.x <= -9)
+                        Mobs[3].transform.position += new Vector3(0.5f, 0, 0);
+
+                    if (Mobs[4].transform.position.x <= -6)
+                        Mobs[4].transform.position += new Vector3(0.5f, 0, 0);
+
+                }
+                else if (GameManager.instance.FiverFlag == false)
+                {
+                    if (Mobs[0].transform.position.x <= 15)
+                        Mobs[0].transform.position += new Vector3(0.5f, 0, 0);
+
+                    if (Mobs[1].transform.position.x >= -17)
+                        Mobs[1].transform.position += new Vector3(-0.5f, 0, 0);
+
+                    if (Mobs[2].transform.position.x <= 10)
+                        Mobs[2].transform.position += new Vector3(0.5f, 0, 0);
+
+                    if (Mobs[3].transform.position.x >= -25)
+                        Mobs[3].transform.position += new Vector3(-0.5f, 0, 0);
+
+                    if (Mobs[4].transform.position.x >= -20)
+                        Mobs[4].transform.position += new Vector3(-0.5f, 0, 0);
+                }
                 break;
         }
     }
